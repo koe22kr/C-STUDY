9c28b492594e62a50715a8e46c3b2db46b6fdb76
[KHG] <[koe22kr@gmail.com]>
2019-04-21 22:46:49 +0900

    일요일 끄읕

---
+++ CA-190421/Bank_v06.cpp
@@ -0,0 +1,315 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+
+const int NAME_LEN = 20;
+
+enum { MAKE = 1, DEPOSIT, WITHDRAW, INQUIRE, EXIT };
+
+
+
+//entity
+
+class Account
+{
+private:
+	int ID;
+	int MONEY;
+	char* cusName;
+
+public:
+	
+
+	Account(int inid, int inmoney, const char* inname);
+	Account(const Account &ref);
+	virtual ~Account();
+	int GetAccount() const;
+	void Deposit(int inmoney);
+	void Withdraw(int outmoney);
+	void ShowAccountInfo()const;
+
+	int GetMoney();
+	virtual void intrateMoney()=0;
+
+};
+
+Account::Account(int inid, int inmoney, const char* inname) : ID(inid), MONEY(inmoney)
+{
+	cusName = new char[strlen(inname) + 1];
+	strcpy(cusName, inname);
+
+}
+Account::Account(const Account &ref) : ID(ref.ID), MONEY(ref.MONEY)
+{
+	cusName = new char[strlen(ref.cusName) + 1];
+	strcpy(cusName, ref.cusName);
+}
+Account::~Account()
+{
+	delete cusName;
+}
+int Account::GetAccount() const
+{
+	return ID;
+}
+
+ void Account::Deposit(int inmoney)
+{
+	
+	MONEY += inmoney;
+	cout << "입금성공" << endl;
+}
+ void Account::Withdraw(int outmoney)
+{
+
+	if (outmoney > MONEY)
+	{
+		cout << "잔액이 부족합니다." << endl;
+		return ;
+	}
+	MONEY -= outmoney;
+	cout << "출금성공" << endl;
+	
+}
+
+void Account::ShowAccountInfo() const
+{
+	cout << "계좌번호: " << ID << endl;
+	cout << "이름 : " << cusName << endl;
+	cout << "잔액 : " << MONEY << endl << endl;
+}
+int Account::GetMoney()
+{
+	return MONEY;
+}
+
+
+
+class NomalAccount : public Account
+{
+private:
+	int intrate = 3;
+public:
+	NomalAccount(int inid, int inmoney, const char* inname);
+	virtual 	~NomalAccount();
+
+	virtual void intrateMoney();
+
+};
+
+NomalAccount::NomalAccount(int inid, int inmoney, const char* inname) : Account(inid,inmoney,inname)
+{
+}
+
+NomalAccount::~NomalAccount()
+{
+}
+
+void NomalAccount::intrateMoney()
+{
+	//int money = GetMoney() * intrate / 100;
+	Deposit(GetMoney()*intrate / 100);	
+}
+class GoldAccount :public Account
+{
+private:
+	int intrate = 9;
+	int Membership_Rank = 0;
+	int Membership_intrate[4] = { 0,7,4,2 };// 1,2,3 번 대응. 0번... 은 넘기기 1~3 이외에는 루프하게 만들면 ㄱㅊ
+	
+	virtual void intrateMoney();
+
+
+public:
+	GoldAccount(int inid, int inmoney, const char* inname,int Membership);
+	virtual ~GoldAccount();
+
+
+};
+
+GoldAccount::GoldAccount (int inid, int inmoney, const char* inname, int Membership) : Account(inid,inmoney,inname)
+{
+	Membership_Rank = Membership;
+}
+
+GoldAccount::~GoldAccount()
+{
+}
+
+void GoldAccount::intrateMoney()
+{
+	//int money = GetMoney() * intrate / 100;
+	Deposit(GetMoney()*(intrate + Membership_intrate[Membership_Rank]) / 100);
+}
+
+////////////////////////////////////////////////////////////
+
+class AccountHandler
+{
+private:
+	Account* accArr[100];
+	int accNum;
+
+public:
+	AccountHandler();
+	~AccountHandler();
+
+	void ShowMenu() const;
+	void MakeAccount();
+	void DepositMoney();
+	void WithdrawMoney();
+	void ShowAllAccInfo()const;
+
+
+};
+
+AccountHandler::AccountHandler()
+{
+	accNum = 0;
+}
+AccountHandler::~AccountHandler()
+{
+	for (int i = 0; i < accNum; i++)
+	{
+		delete accArr[i];
+	}
+}
+
+void AccountHandler::ShowMenu() const
+{
+	cout << "------Menu------" << endl;
+	cout << "1.계과개설" << endl;
+	cout << "2.입금" << endl;
+	cout << "3.출금" << endl;
+	cout << "4.계좌정보 전체 출력" << endl;
+	cout << "5.프로그램 종료" << endl;
+}
+void AccountHandler::MakeAccount()
+{
+	int newid;
+	int newmoney;
+	char newname[20];
+	int memrank = 0;
+	int Accounttype = 0;
+	cout << "[계좌 종류 선택]" << endl << "1.일반계좌 2.골드계좌" << endl;
+	cin >> Accounttype;
+	switch (Accounttype)
+	{
+	case 1:
+		cout << "일반계좌 생성" << endl;
+		cout << "계좌번호 :";
+		cin >> newid;
+		cout << "초기 입금액 :";
+		cin >> newmoney;
+		cout << "이름 :";
+		cin >> newname;
+		accArr[accNum++] = new NomalAccount(newid, newmoney, newname);
+		break;
+	case 2:
+		
+		cout << "골드계좌 생성" << endl;
+		cout << "계좌번호 :";
+		cin >> newid;
+		cout << "초기 입금액 :";
+		cin >> newmoney;
+		cout << "이름 :";
+		cin >> newname;
+		while (memrank != (1 || 2 || 3))
+		{
+			cout << "맴버쉽 랭크를 입력해주세요.(A=1,B=2,C=3) :";
+			cin >> memrank;
+		}		
+		accArr[accNum++] = new GoldAccount(newid, newmoney, newname, memrank);
+		break;
+	default:
+		cout << "잘못된 숫자를 입력." << endl;
+		break;
+	}
+	
+	
+
+	
+}
+void AccountHandler::DepositMoney()
+{
+	int inmoney;
+	int inid;
+	cout << "입금할 계좌번호 : ";
+	cin >> inid;
+	cout << "입금할 금액 :";
+	cin >> inmoney;
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i]->GetAccount() == inid)
+		{
+			accArr[i]->intrateMoney();
+			accArr[i]->Deposit(inmoney);
+			return;
+		}
+	}
+	cout << "해당 계좌를 찾지 못하였습니다." << endl;
+}
+
+void AccountHandler::WithdrawMoney()
+{
+	int outmoney;
+	int outid;
+	cout << "출금할 계좌번호 : ";
+	cin >> outid;
+	cout << "출금할 금액 :";
+	cin >> outmoney;
+	for (int i = 0; i < accNum; i++)
+	{
+		if (accArr[i]->GetAccount() == outid)
+		{
+			accArr[i]->Withdraw(outmoney);
+			return;
+		}
+	}
+	cout << "해당 계좌를 찾지 못하였습니다." << endl;
+}
+
+void AccountHandler::ShowAllAccInfo() const
+{
+	for (int i = 0; i < accNum; i++)
+	{
+		accArr[i]->ShowAccountInfo();
+	}
+}
+
+int main()
+{
+	AccountHandler Mgr;
+	int choice;
+	while (true)
+	{
+		Mgr.ShowMenu();
+		cout << "선택 : ";
+		cin >> choice;
+		cout << endl;
+
+		switch (choice)
+		{
+		case MAKE:
+			Mgr.MakeAccount();
+			break;
+		case DEPOSIT:
+			Mgr.DepositMoney();
+			break;
+		case WITHDRAW:
+			Mgr.WithdrawMoney();
+			break;
+		case INQUIRE:
+			Mgr.ShowAllAccInfo();
+			break;
+		case EXIT:
+			return 0;
+		default:
+			cout << "잘못된 숫자를 입력." << endl;
+		}
+
+
+	}
+	return 0;
+}
