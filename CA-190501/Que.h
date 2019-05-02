33c0fba36e470b7d8d7eb5d587fa190e14c53659
[KHG] <[koe22kr@gmail.com]>
2019-05-02 11:00:48 +0900

    Revert "Revert "Revert "khg"""
    This reverts commit 5ff7f0fd5828449d6926657c58c8ba9075816af8.

--- CA-190501/Que.h
+++ CA-190501/Que.h
@@ -1,18 +1,7 @@
 #pragma once
-#include "Tlinklist.h"
+#include ""
 
-class dummy
-{
-public:
-	int index = 0;
-	int data = 0;
-	dummy() {};
-	dummy(int a,int b):index(a),data(b)
-	{
 
-	};
-	~dummy() {};
-};
 
 class Que
 {
@@ -20,51 +9,19 @@ class Que
 public:
 	Que();
 	~Que();
-
-	TLinklist<dummy>* list;
-public:
-	dummy Front();
-	dummy Back();
-	void Push_back(dummy data);
+	Tlinklist list;
+	void Front();
+	void Back();
+	void Push_back();
 	void Pop();
 	void clear();
 	void Size();
 
 };
 
-Que::Que()
+void Que::Front()
 {
-	list = new TLinklist<dummy>;
-}
 
 
-Que::~Que()
-{
-}
-
-dummy Que::Front()
-{
-	if (list->GetFirst() == list->GetTail())
-	{
-		cout << "Front==Tail" << endl;
-//		return list->Getdata(list->GetTail());
-	}
-	return list->Getdata(list->GetFirst());
-}
-
-dummy Que::Back()
-{
-	if (list->GetLast() == list->GetHead())
-	{
-		cout << "Back==Head" << endl;
-		//return list->Getdata(list->GetHead());
-	}
-	return list->Getdata(list->GetLast());
-}	
-
-void Que::Push_back(dummy data)
-{
-	Tnode<dummy>* temp = new Tnode<dummy>;
-	temp = list->Add_Link_Back();
 
 }
